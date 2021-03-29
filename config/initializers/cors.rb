249cab0d37f41bad586b99c207a40729265652b8
naderbenchaaben <naderbenchaaben@gmail.com>
2021-03-29 12:22:19 +0100

    adding models

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # Make sure to change the * to the location of you frontend after deployment in order to secure your application
   resource '*',
                headers: :any,
-               methods: %i[get post put patch delete options head]
+               methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_043116) do
+ActiveRecord::Schema.define(version: 2021_03_29_044246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "path"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
