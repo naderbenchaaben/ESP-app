f750ef08da3d2bc3181fe879ef3dbd304840d835
naderbenchaaben <naderbenchaaben@gmail.com>
2021-04-19 08:35:26 +0100

    multi-origin-cors

--- app/controllers/api/v2/products_controller.rb
+++ app/controllers/api/v2/products_controller.rb
@@ -1,80 +1,40 @@
 class Api::V2::ProductsController < ApplicationController
- before_action :set_product, only: %i[ create show edit update destroy ]
+# before_action :set_product, only: %i[ create show edit update destroy ]
 
-  # GET /products or /products.json
-  def index
-    @products = Product.all
-    render json: @product
-  end
-
-  # GET /products/1 or /products/1.json
-  def show
-    @product = set_product
-    render json: @product
-  end
-
-  # GET /products/new
-  def new
-    @product = Product.new
-  end
-
-  # GET /products/1/edit
-  def edit
-  end
+  
 
   # POST /products or /products.json
   def create
-    product = Product.new(product_params)
-    respond_to :json
-    
-      if product.save
-        #format.html { redirect_to @order, notice: "Order was successfully created." }
-       # render json: {  :show, status: :created, location: @rating}
-       render json:product
-      else
-        #format.html { render :new, status: :unprocessable_entity }
-        #format.json { render json: @rating.errors, status: :unprocessable_entity }
-        render json: {error: products.errors.messages }
-      
-    end
-  end
-
-  # PATCH/PUT /products/1 or /products/1.json
-  def update
-    @product = set_product
-    if @product.update(company_params)
-      #format.html { redirect_to @category, notice: "Category was successfully updated." }
-      render json:@product, status: :ok
+    product = Product.create!(
+      #email: params[:user][:email],
+      product_name: params[:product][:product_name],
+      ref_product: params[:product][:ref_product],
+      price: params[:product][:price],
+      shortDesc: params[:product] [:shortDesc],
+      description: params[:product][:description],
+      available_quantity: params[:product][:available_quantity]
+    )
+    if product
+      render json:{
+        status: :created,
+        product: product
+      }
     else
-      #format.html { render :edit, status: :unprocessable_entity }
-      render json: {error: @products.errors.messages }
-  
-  end
-  end
-
-  # DELETE /products/1 or /products/1.json
-  def destroy
-    @product= set_product
-       if @product.destroy  
-      head :no_content
-       # format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      else
-        render json: {error: @products.error.messege }
-      end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
+      render json: { status: 500 }
     end
-
-    # Only allow a list of trusted parameters through.
-    def product_params
-<<<<<<< HEAD
-      params.require(:product).permit(:product_name, :ref_product, :price, :shortDes, :description, :available_quantity )
-=======
-      params.permit(:product_name, :ref_product, :price, :description, :available_quantity )
->>>>>>> FIX-register-user
+  end
+   def update
+    product =Product.update!(
+      product_name: params[:product][:product_name],
+      ref_product: params[:product][:ref_product],
+      price: params[:product][:price],
+      shortDesc: params[:product] [:shortDesc],
+      description: params[:product][:description],
+      available_quantity: params[:product][:available_quantity]
+    )
+   end
+    private
+      def set_product
+      product = Product.find(params[:id])
     end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,19 +1,14 @@
 
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-<<<<<<< HEAD
-    allow do
-      origins 'http://localhost:3001'
-  # Make sure to change the * to the location of you frontend after deployment in order to secure your application
-  resource '*',
-               headers: :any,
-               methods: %i[get post put patch delete options head],
-               credentials: true
-    end
-  end
-=======
+
   allow do
     origins "http://localhost:3000"
+    resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+    credentials: true #allow you to pass the cookie from fontend to backkend app
+  end
+  allow do
+    origins "http://localhost:19006"
     resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
     credentials: true #allow you to pass the cookie from fontend to backkend app
   end
@@ -25,4 +20,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   #  end
   
 end
->>>>>>> FIX-register-user
+
